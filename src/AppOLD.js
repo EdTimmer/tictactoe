31f849a77a3dca9ab5fab6a8d7a6ce5d1a32ecc6
Edward Timmer <edtimmer@gmail.com>
2019-03-22 19:34:13 -0400

    working on autoplay

--- src/AppOLD.js
+++ src/AppOLD.js
@@ -5,15 +5,15 @@ import o from './images/o.png';
 import './App.css';
 
 const App = () => {
-  const [box1Sign, setBox1Sign] = useState("");
-  const [box2Sign, setBox2Sign] = useState("");
-  const [box3Sign, setBox3Sign] = useState("");
-  const [box4Sign, setBox4Sign] = useState("");
-  const [box5Sign, setBox5Sign] = useState("");
-  const [box6Sign, setBox6Sign] = useState("");
-  const [box7Sign, setBox7Sign] = useState("");
-  const [box8Sign, setBox8Sign] = useState("");
-  const [box9Sign, setBox9Sign] = useState("");
+  const [box1sign, setBox1sign] = useState("");
+  const [box2sign, setBox2sign] = useState("");
+  const [box3sign, setBox3sign] = useState("");
+  const [box4sign, setBox4sign] = useState("");
+  const [box5sign, setBox5sign] = useState("");
+  const [box6sign, setBox6sign] = useState("");
+  const [box7sign, setBox7sign] = useState("");
+  const [box8sign, setBox8sign] = useState("");
+  const [box9sign, setBox9sign] = useState("");
 
   const [box1HasMove, setBox1HasMove] = useState(true);
   const [box2HasMove, setBox2HasMove] = useState(true);
@@ -27,13 +27,16 @@ const App = () => {
 
   const [flipSign, setFlipSign] = useState(true);
 
+  // const [gameOver, setGameOver] = useState(false);
+
   // const [message, setMessage] = useState("");
 
   let message = "";
+  let gameOver = false;
 
   const mark = (n) => {
     
-    const boxesArr = [setBox1Sign, setBox2Sign, setBox3Sign, setBox4Sign, setBox5Sign, setBox6Sign, setBox7Sign, setBox8Sign, setBox9Sign];
+    const boxesArr = [setBox1sign, setBox2sign, setBox3sign, setBox4sign, setBox5sign, setBox6sign, setBox7sign, setBox8sign, setBox9sign];
     const signFunc = boxesArr[n-1];
 
     const boxHasMoveArr = [box1HasMove, box2HasMove, box3HasMove, box4HasMove, box5HasMove, box6HasMove, box7HasMove, box8HasMove, box9HasMove];
@@ -41,9 +44,6 @@ const App = () => {
 
     const changeHasMoveArr = [setBox1HasMove, setBox2HasMove, setBox3HasMove, setBox4HasMove, setBox5HasMove, setBox6HasMove, setBox7HasMove, setBox8HasMove, setBox9HasMove];
     const changeHasMoveFunc = changeHasMoveArr[n - 1];
-
-    const initialBoxesArr = [setBox1Sign, setBox2Sign, setBox3Sign, setBox4Sign, setBox5Sign, setBox6Sign, setBox7Sign, setBox8Sign, setBox9Sign];
-    const initialChangeHasMoveArr = [box1HasMove, box2HasMove, box3HasMove, box4HasMove, box5HasMove, box6HasMove, box7HasMove, box8HasMove, box9HasMove];
 
     
     if (hasMove && flipSign) {      
@@ -58,21 +58,21 @@ const App = () => {
       setFlipSign(true);
     }
 
-    console.log('box1Sign is: ', box1Sign)
+    console.log('box1sign is: ', box1sign)
 
   }
 
   const reset = () => {
     document.getElementById('boxes').style.pointerEvents = 'auto';
-    setBox1Sign("");
-    setBox2Sign("");
-    setBox3Sign("");
-    setBox4Sign("");
-    setBox5Sign("");
-    setBox6Sign("");
-    setBox7Sign("");
-    setBox8Sign("");
-    setBox9Sign("");
+    setBox1sign("");
+    setBox2sign("");
+    setBox3sign("");
+    setBox4sign("");
+    setBox5sign("");
+    setBox6sign("");
+    setBox7sign("");
+    setBox8sign("");
+    setBox9sign("");
   
     setBox1HasMove(true);
     setBox2HasMove(true);
@@ -87,41 +87,49 @@ const App = () => {
     setFlipSign(true);
   }
 
-  //horizontal win
-  if (box1Sign !== "" && box1Sign === box2Sign && box2Sign === box3Sign) {
-    message = box1Sign === x ? "X Wins!" : "O Wins!";
+//horizontal win
+  if (box1sign !== "" && box1sign === box2sign && box2sign === box3sign) {
+    message = box1sign === x ? "X Wins!" : "O Wins!";
+    gameOver= true;
     document.getElementById('boxes').style.pointerEvents = 'none';
   }
-  if (box4Sign !== "" && box4Sign === box5Sign && box5Sign === box6Sign) {
-    message = box4Sign === x ? "X Wins!" : "O Wins!";
+  if (box4sign !== "" && box4sign === box5sign && box5sign === box6sign) {
+    message = box4sign === x ? "X Wins!" : "O Wins!";
+    gameOver= true;
     document.getElementById('boxes').style.pointerEvents = 'none';
   }
-  if (box7Sign !== "" && box7Sign === box8Sign && box8Sign === box9Sign) {
-    message = box7Sign === x ? "X Wins!" : "O Wins!";;
+  if (box7sign !== "" && box7sign === box8sign && box8sign === box9sign) {
+    message = box7sign === x ? "X Wins!" : "O Wins!";;
+    gameOver= true;
     document.getElementById('boxes').style.pointerEvents = 'none';
   }
 
   //vertical win
-  if (box1Sign !== "" && box1Sign === box4Sign && box4Sign === box7Sign) {
-    message = box1Sign === x ? "X Wins!" : "O Wins!";
+  if (box1sign !== "" && box1sign === box4sign && box4sign === box7sign) {
+    message = box1sign === x ? "X Wins!" : "O Wins!";
+    gameOver= true;
     document.getElementById('boxes').style.pointerEvents = 'none';
   }
-  if (box2Sign !== "" && box2Sign === box5Sign && box5Sign === box8Sign) {
-    message = box2Sign === x ? "X Wins!" : "O Wins!";
+  if (box2sign !== "" && box2sign === box5sign && box5sign === box8sign) {
+    message = box2sign === x ? "X Wins!" : "O Wins!";
+    gameOver= true;
     document.getElementById('boxes').style.pointerEvents = 'none';
   }
-  if (box3Sign !== "" && box3Sign === box6Sign && box6Sign === box9Sign) {
-    message = box3Sign === x ? "X Wins!" : "O Wins!";
+  if (box3sign !== "" && box3sign === box6sign && box6sign === box9sign) {
+    message = box3sign === x ? "X Wins!" : "O Wins!";
+    gameOver= true;
     document.getElementById('boxes').style.pointerEvents = 'none';
   }
 
   //diagonal win
-  if (box1Sign !== "" && box1Sign === box5Sign && box5Sign === box9Sign) {
-    message = box1Sign === x ? "X Wins!" : "O Wins!";    
+  if (box1sign !== "" && box1sign === box5sign && box5sign === box9sign) {
+    message = box1sign === x ? "X Wins!" : "O Wins!";
+    gameOver= true;
     document.getElementById('boxes').style.pointerEvents = 'none';
   }
-  if (box3Sign !== "" && box3Sign === box5Sign && box5Sign === box7Sign) {
-    message = box3Sign === x ? "X Wins!" : "O Wins!";    
+  if (box3sign !== "" && box3sign === box5sign && box5sign === box7sign) {
+    message = box3sign === x ? "X Wins!" : "O Wins!";;
+    gameOver= true;
     document.getElementById('boxes').style.pointerEvents = 'none';
   }
 
@@ -132,51 +140,51 @@ const App = () => {
 
         <div className="grid-item box1" onClick={() => mark(1)}>        
           {
-            box1Sign ? <img src={box1Sign} className="image" alt="sign" /> : null
+            box1sign ? <img src={box1sign} className="image" alt="sign" /> : null
           }        
         </div>
 
         <div className="grid-item box2" onClick={() => mark(2)}>
           {
-            box2Sign ? <img src={box2Sign} className="image" alt="sign" /> : null
+            box2sign ? <img src={box2sign} className="image" alt="sign" /> : null
           }        
         </div>
 
         <div className="grid-item box3" onClick={() => mark(3)}>
           {
-            box3Sign ? <img src={box3Sign} className="image" alt="sign" /> : null
+            box3sign ? <img src={box3sign} className="image" alt="sign" /> : null
           }        
         </div>
 
         <div className="grid-item box4" onClick={() => mark(4)}>
           {
-            box4Sign ? <img src={box4Sign} className="image" alt="sign" /> : null
+            box4sign ? <img src={box4sign} className="image" alt="sign" /> : null
           }        
         </div>
         <div className="grid-item box5" onClick={() => mark(5)}>
           {
-            box5Sign ? <img src={box5Sign} className="image" alt="sign" /> : null
+            box5sign ? <img src={box5sign} className="image" alt="sign" /> : null
           }        
         </div>
         <div className="grid-item box6" onClick={() => mark(6)}>
           {
-            box6Sign ? <img src={box6Sign} className="image" alt="sign" /> : null
+            box6sign ? <img src={box6sign} className="image" alt="sign" /> : null
           }        
         </div>
 
         <div className="grid-item box7" onClick={() => mark(7)}>
           {
-            box7Sign ? <img src={box7Sign} className="image" alt="sign" /> : null
+            box7sign ? <img src={box7sign} className="image" alt="sign" /> : null
           }        
         </div>
         <div className="grid-item box8" onClick={() => mark(8)}>
           {
-            box8Sign ? <img src={box8Sign} className="image" alt="sign" /> : null
+            box8sign ? <img src={box8sign} className="image" alt="sign" /> : null
           }        
         </div>
         <div className="grid-item box9" onClick={() => mark(9)}>
           {
-            box9Sign ? <img src={box9Sign} className="image" alt="sign" /> : null
+            box9sign ? <img src={box9sign} className="image" alt="sign" /> : null
           }        
         </div>        
       
