1e2739ae80878d5bd4921af5a19ebf389f9c35d7
Edward Timmer <edtimmer@gmail.com>
2019-03-21 20:28:21 -0400

    game is working

--- src/App.js
+++ src/App.js
@@ -27,9 +27,12 @@ const App = () => {
 
   const [flipSign, setFlipSign] = useState(true);
 
+  // const [gameOver, setGameOver] = useState(false);
+
   // const [message, setMessage] = useState("");
 
   let message = "";
+  let gameOver = false;
 
   const mark = (n) => {
     
@@ -55,83 +58,141 @@ const App = () => {
       setFlipSign(true);
     }
 
+    console.log('box1sign is: ', box1sign)
+
   }
 
-  if (box1sign === x && box2sign === x && box3sign === x) {
-    // setMessage("X Victory!")
-    message = "X Victory!";
-    // setGameOver
+  const reset = () => {
+    document.getElementById('boxes').style.pointerEvents = 'auto';
+    setBox1sign("");
+    setBox2sign("");
+    setBox3sign("");
+    setBox4sign("");
+    setBox5sign("");
+    setBox6sign("");
+    setBox7sign("");
+    setBox8sign("");
+    setBox9sign("");
+  
+    setBox1HasMove(true);
+    setBox2HasMove(true);
+    setBox3HasMove(true);
+    setBox4HasMove(true);
+    setBox5HasMove(true);
+    setBox6HasMove(true);
+    setBox7HasMove(true);
+    setBox8HasMove(true);
+    setBox9HasMove(true);
+
+    setFlipSign(true);
   }
-  // if (box4sign === x && box4sign === box5sign && box5sign === box6sign) {
-  //   setMessage("X Victory!")
-  //   // message = "X Victory!"
-  // }
-  // if (box7sign === x && box7sign === box8sign && box8sign === box9sign) {
-  //   setMessage("X Victory!")
-  //   // message = "X Victory!"
-  // }
 
-  // console.log(message)
+//horizontal win
+  if (box1sign !== "" && box1sign === box2sign && box2sign === box3sign) {
+    message = "Victory!";
+    gameOver= true;
+    document.getElementById('boxes').style.pointerEvents = 'none';
+  }
+  if (box4sign !== "" && box4sign === box5sign && box5sign === box6sign) {
+    message = "Victory!";
+    gameOver= true;
+    document.getElementById('boxes').style.pointerEvents = 'none';
+  }
+  if (box7sign !== "" && box7sign === box8sign && box8sign === box9sign) {
+    message = "Victory!";
+    gameOver= true;
+    document.getElementById('boxes').style.pointerEvents = 'none';
+  }
 
+  //vertical win
+  if (box1sign !== "" && box1sign === box4sign && box4sign === box7sign) {
+    message = "Victory!";
+    gameOver= true;
+    document.getElementById('boxes').style.pointerEvents = 'none';
+  }
+  if (box2sign !== "" && box2sign === box5sign && box5sign === box8sign) {
+    message = "Victory!";
+    gameOver= true;
+    document.getElementById('boxes').style.pointerEvents = 'none';
+  }
+  if (box3sign !== "" && box3sign === box6sign && box6sign === box9sign) {
+    message = "Victory!";
+    gameOver= true;
+    document.getElementById('boxes').style.pointerEvents = 'none';
+  }
+
+  //diagonal win
+  if (box1sign !== "" && box1sign === box5sign && box5sign === box9sign) {
+    message = "Victory!";
+    gameOver= true;
+    document.getElementById('boxes').style.pointerEvents = 'none';
+  }
+  if (box3sign !== "" && box3sign === box5sign && box5sign === box7sign) {
+    message = "Victory!";
+    gameOver= true;
+    document.getElementById('boxes').style.pointerEvents = 'none';
+  }
 
   return (
     <div className="App">
 
-      <div className="grid-container">
+      <div id="boxes" className="grid-container">
 
-        <div className="grid-item box1" onClick={() => mark(1)} disabled={message.length > 1}>        
+        <div className="grid-item box1" onClick={() => mark(1)}>        
           {
-            box1sign ? <img src={box1sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
-          }        
-        </div>
-
-        <div className="grid-item box2" onClick={() => mark(2)} disabled={message.length > 1}>
-          {
-            box2sign ? <img src={box2sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
+            box1sign ? <img src={box1sign} className="image" alt="sign" /> : null
           }        
         </div>
 
-        <div className="grid-item box3" onClick={() => mark(3)} disabled={message.length > 1}>
+        <div className="grid-item box2" onClick={() => mark(2)}>
           {
-            box3sign ? <img src={box3sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
+            box2sign ? <img src={box2sign} className="image" alt="sign" /> : null
           }        
         </div>
 
-        <div className="grid-item box4" onClick={() => mark(4)} disabled={message.length > 1}>
+        <div className="grid-item box3" onClick={() => mark(3)}>
           {
-            box4sign ? <img src={box4sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box5" onClick={() => mark(5)} disabled={message.length > 1}>
-          {
-            box5sign ? <img src={box5sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box6" onClick={() => mark(6)} disabled={message.length > 1}>
-          {
-            box6sign ? <img src={box6sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
+            box3sign ? <img src={box3sign} className="image" alt="sign" /> : null
           }        
         </div>
 
-        <div className="grid-item box7" onClick={() => mark(7)} disabled={message.length > 1}>
+        <div className="grid-item box4" onClick={() => mark(4)}>
           {
-            box7sign ? <img src={box7sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
+            box4sign ? <img src={box4sign} className="image" alt="sign" /> : null
           }        
         </div>
-        <div className="grid-item box8" onClick={() => mark(8)} disabled={message.length > 1}>
+        <div className="grid-item box5" onClick={() => mark(5)}>
           {
-            box8sign ? <img src={box8sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
+            box5sign ? <img src={box5sign} className="image" alt="sign" /> : null
           }        
         </div>
-        <div className="grid-item box9" onClick={() => mark(9)} disabled={message.length > 1}>
+        <div className="grid-item box6" onClick={() => mark(6)}>
           {
-            box9sign ? <img src={box9sign} style={{ height: '5rem', width: '5rem' }} alt="sign" /> : null
+            box6sign ? <img src={box6sign} className="image" alt="sign" /> : null
+          }        
+        </div>
+
+        <div className="grid-item box7" onClick={() => mark(7)}>
+          {
+            box7sign ? <img src={box7sign} className="image" alt="sign" /> : null
+          }        
+        </div>
+        <div className="grid-item box8" onClick={() => mark(8)}>
+          {
+            box8sign ? <img src={box8sign} className="image" alt="sign" /> : null
+          }        
+        </div>
+        <div className="grid-item box9" onClick={() => mark(9)}>
+          {
+            box9sign ? <img src={box9sign} className="image" alt="sign" /> : null
           }        
         </div>        
       
       </div>
 
-      <div>{message}</div>
+      <div className="message">{message}</div>
+
+      <button onClick={reset}>RESET</button>
 
     </div>
   );
