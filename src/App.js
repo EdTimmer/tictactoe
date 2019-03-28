4a1328b0f199df1ed7518ac4b39bb354916719dc
Edward Timmer <edtimmer@gmail.com>
2019-03-27 23:11:06 -0400

    frame styling

--- src/App.js
+++ src/App.js
@@ -162,66 +162,72 @@ const App = () => {
   return (
     <div className="App">
 
-      <div className="inner-container">
-      
-      
 
-      <div className="logo"><img className="logo-image" src={logo} alt="logo" /></div>
 
-      <div id="boxes" className="grid-container">
+      <div className="main5">
 
-        <div className="grid-item box1" onClick={() => xMove(1)}>        
-          {
-            box1Sign ? <img src={box1Sign} className="image" alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box2" onClick={() => xMove(2)}>
-          {
-            box2Sign ? <img src={box2Sign} className="image" alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box3" onClick={() => xMove(3)}>
-          {
-            box3Sign ? <img src={box3Sign} className="image" alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box4" onClick={() => xMove(4)}>
-          {
-            box4Sign ? <img src={box4Sign} className="image" alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box5" onClick={() => xMove(5)}>
-          {
-            box5Sign ? <img src={box5Sign} className="image" alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box6" onClick={() => xMove(6)}>
-          {
-            box6Sign ? <img src={box6Sign} className="image" alt="sign" /> : null
-          }        
+        <div className="inner-container">
+        
+        
+
+        <div className="logo"><img className="logo-image" src={logo} alt="logo" /></div>
+
+        <div id="boxes" className="grid-container">
+
+          <div className="grid-item box1" onClick={() => xMove(1)}>        
+            {
+              box1Sign ? <img src={box1Sign} className="image" alt="sign" /> : null
+            }        
+          </div>
+          <div className="grid-item box2" onClick={() => xMove(2)}>
+            {
+              box2Sign ? <img src={box2Sign} className="image" alt="sign" /> : null
+            }        
+          </div>
+          <div className="grid-item box3" onClick={() => xMove(3)}>
+            {
+              box3Sign ? <img src={box3Sign} className="image" alt="sign" /> : null
+            }        
+          </div>
+          <div className="grid-item box4" onClick={() => xMove(4)}>
+            {
+              box4Sign ? <img src={box4Sign} className="image" alt="sign" /> : null
+            }        
+          </div>
+          <div className="grid-item box5" onClick={() => xMove(5)}>
+            {
+              box5Sign ? <img src={box5Sign} className="image" alt="sign" /> : null
+            }        
+          </div>
+          <div className="grid-item box6" onClick={() => xMove(6)}>
+            {
+              box6Sign ? <img src={box6Sign} className="image" alt="sign" /> : null
+            }        
+          </div>
+
+          <div className="grid-item box7" onClick={() => xMove(7)}>
+            {
+              box7Sign ? <img src={box7Sign} className="image" alt="sign" /> : null
+            }        
+          </div>
+          <div className="grid-item box8" onClick={() => xMove(8)}>
+            {
+              box8Sign ? <img src={box8Sign} className="image" alt="sign" /> : null
+            }        
+          </div>
+          <div className="grid-item box9" onClick={() => xMove(9)}>
+            {
+              box9Sign ? <img src={box9Sign} className="image" alt="sign" /> : null
+            }        
+          </div>        
+        
         </div>
 
-        <div className="grid-item box7" onClick={() => xMove(7)}>
-          {
-            box7Sign ? <img src={box7Sign} className="image" alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box8" onClick={() => xMove(8)}>
-          {
-            box8Sign ? <img src={box8Sign} className="image" alt="sign" /> : null
-          }        
-        </div>
-        <div className="grid-item box9" onClick={() => xMove(9)}>
-          {
-            box9Sign ? <img src={box9Sign} className="image" alt="sign" /> : null
-          }        
-        </div>        
-      
-      </div>
+        <div className="message">{message}</div>
 
-      <div className="message">{message}</div>
+        <div className="reset-button" onClick={reset}>RESET</div>
 
-      <div className="reset-button" onClick={reset}>RESET</div>
+        </div>
 
       </div>
 
